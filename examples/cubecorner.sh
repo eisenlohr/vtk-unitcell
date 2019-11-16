cf93cc9f7b9c79703758cf8d11507039780996ce
Philip Eisenlohr <eisenlohr@egr.msu.edu>
2019-11-15 19:10:02 -0500

    simple example to generate a stack of cubes rotated on a corner

---
+++ examples/cubecorner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+for i in {0..10}
+do
+  delta=$(( 2*$i ))
+  $DIR/../vtk_unitcell.py \
+    --axisangle 1 1 1 $(( 120/10*$i )) \
+    --degrees \
+    --family cubic \
+    --position $delta $delta $delta \
+  > cubecorner_$i.vtk  
+  
+done
